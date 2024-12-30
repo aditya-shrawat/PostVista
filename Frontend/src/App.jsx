b7d625af84c0a274ed6390fec87ad1971cf615ae
aditya-shrawat <adityashrawat.social@gmail.com>
2024-12-30 20:15:20 +0530

    initial setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Layout from './Components/Layout'
+import HomePage from './Pages/HomePage'
+import PostDetailPage from './Pages/PostDetailPage'
+import ProfilePage from './Pages/ProfilePage'
+import Errorpage from './Pages/Errorpage'
+import LoginPage from './Pages/LoginPage'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout />,
+    children:[
+      {path:'/', element: <HomePage /> },
+      {path:'login',element:<LoginPage /> },
+      {path:'signup',element:<LoginPage /> },
+      {path:'post/:id',element:<PostDetailPage /> },
+      {path:'profile/:id',element:<ProfilePage /> },
+    ]
+  },
+  {
+    path:'*',
+    element:<Errorpage />
+  }
+])
+
+
+const App = () => {
+  return <RouterProvider router={router} />
+}
+
+export default App
